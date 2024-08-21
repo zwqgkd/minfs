3fa6dbb68a4c36b401cca543241ceec7012f64af
zhang wenqiang <zhangwenqiang@kingsoft.com>
2024-08-21 11:56:16 +0800

    fix部署

--- easyClient/src/main/java/com/ksyun/campus/client/TestFunc.java
+++ easyClient/src/main/java/com/ksyun/campus/client/TestFunc.java
@@ -18,82 +18,81 @@ public class TestFunc {
 
 
 
-//        // mkdir功能
-//        eFileSystem.mkdir("/test/newDir/");
-//        eFileSystem.mkdir("/test/deleteDir/");
-//
-//        // create功能
-//        FSOutputStream fsOutputStream1 = eFileSystem.create("/test/newDir/File1.txt");
-//        FSOutputStream fsOutputStream2 = eFileSystem.create("/test/deleteDir/File2.txt");
-//        fsOutputStream2.close();
-//
-//        // write功能
-//        String charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':,.<>/?`~";
-//        Random random = new Random();
-//
-//        // 创建一个StringBuilder来生成字符串
-//        StringBuilder sb = new StringBuilder();
-//
-//        // 根据需要的大小生成字符串
-//        int targetLength = 1 * 10 * 1024;
-//        while (sb.length() < targetLength) {
-//            sb.append(charset.charAt(random.nextInt(charset.length())));
-//        }
-//
-//        // 将生成的字符串转换为UTF-8字节数组
-//        byte[] byteArray = sb.toString().getBytes(StandardCharsets.UTF_8);
-//
-//        fsOutputStream1.write(byteArray);
-//        fsOutputStream1.close();
-//
-//        // 计算MD5
-//        MessageDigest md = MessageDigest.getInstance("MD5");
-//        byte[] md5Bytes = md.digest(byteArray);
-//
-//        // 转换为十六进制字符串
-//        StringBuilder md5Hex = new StringBuilder();
-//        for (byte b : md5Bytes) {
-//            md5Hex.append(String.format("%02x", b));
-//        }
-//
-//        System.out.println("write MD5: " + md5Hex.toString());
-//
-//        FSInputStream fsInputStream = eFileSystem.open("/test/newDir/File1.txt");
-//        byte[] buf = new byte[1024];
-//        StringBuilder res = new StringBuilder();
-//
-//        int bytesRead = 0;
-//        while ((bytesRead = fsInputStream.read(buf)) != -1) {
-//            System.out.println(bytesRead);
-//            String data = new String(buf, 0, bytesRead);
-//            System.out.println("Read data: " + data);
-//            res.append(data);
-//        }
-//        fsInputStream.close();
-//
-//        // 计算MD5
-//        MessageDigest mdr = MessageDigest.getInstance("MD5");
-//        byte[] md5Bytesr = md.digest(res.toString().getBytes());
-//
-//        // 转换为十六进制字符串
-//        StringBuilder md5Hexr = new StringBuilder();
-//        for (byte b : md5Bytesr) {
-//            md5Hexr.append(String.format("%02x", b));
-//        }
-//
-//        System.out.println("read MD5: " + md5Hexr.toString());
-//        System.out.println("Compare write and read is : " + md5Hexr.toString().contentEquals(md5Hex));
-//
-//        // delete功能
-//        eFileSystem.delete("/test/deleteDir/");
-//
-//        // 有问题
-//        // System.out.println(eFileSystem.getClusterInfo().toString());
-//        System.out.println(eFileSystem.getFileStats("/test/newDir/File1.txt"));
-//        List<StatInfo> ll = eFileSystem.listFileStats("/test/");
-//        for (StatInfo statInfo : ll) {
-//            System.out.println(statInfo);
-//        }
+        // mkdir功能
+        eFileSystem.mkdir("/test/newDir/");
+        eFileSystem.mkdir("/test/deleteDir/");
+
+        // create功能
+        FSOutputStream fsOutputStream1 = eFileSystem.create("/test/newDir/File1.txt");
+        FSOutputStream fsOutputStream2 = eFileSystem.create("/test/deleteDir/File2.txt");
+        fsOutputStream2.close();
+
+        // write功能
+        String charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;':,.<>/?`~";
+        Random random = new Random();
+
+        // 创建一个StringBuilder来生成字符串
+        StringBuilder sb = new StringBuilder();
+
+        // 根据需要的大小生成字符串
+        int targetLength = 1 * 100 * 1024;
+        while (sb.length() < targetLength) {
+            sb.append(charset.charAt(random.nextInt(charset.length())));
+        }
+
+        // 将生成的字符串转换为UTF-8字节数组
+        byte[] byteArray = sb.toString().getBytes(StandardCharsets.UTF_8);
+
+        fsOutputStream1.write(byteArray);
+        fsOutputStream1.close();
+
+        // 计算MD5
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] md5Bytes = md.digest(byteArray);
+
+        // 转换为十六进制字符串
+        StringBuilder md5Hex = new StringBuilder();
+        for (byte b : md5Bytes) {
+            md5Hex.append(String.format("%02x", b));
+        }
+
+        System.out.println("write MD5: " + md5Hex.toString());
+
+        FSInputStream fsInputStream = eFileSystem.open("/test/newDir/File1.txt");
+        byte[] buf = new byte[1024];
+        StringBuilder res = new StringBuilder();
+
+        int bytesRead = 0;
+        while ((bytesRead = fsInputStream.read(buf)) != -1) {
+            System.out.println(bytesRead);
+            String data = new String(buf, 0, bytesRead);
+            System.out.println("Read data: " + data);
+            res.append(data);
+        }
+        fsInputStream.close();
+
+        // 计算MD5
+        MessageDigest mdr = MessageDigest.getInstance("MD5");
+        byte[] md5Bytesr = md.digest(res.toString().getBytes());
+
+        // 转换为十六进制字符串
+        StringBuilder md5Hexr = new StringBuilder();
+        for (byte b : md5Bytesr) {
+            md5Hexr.append(String.format("%02x", b));
+        }
+
+        System.out.println("read MD5: " + md5Hexr.toString());
+        System.out.println("Compare write and read is : " + md5Hexr.toString().contentEquals(md5Hex));
+
+        // delete功能
+        eFileSystem.delete("/test/deleteDir/");
+
+        // System.out.println(eFileSystem.getClusterInfo().toString());
+        System.out.println(eFileSystem.getFileStats("/test/newDir/File1.txt"));
+        List<StatInfo> ll = eFileSystem.listFileStats("/test/");
+        for (StatInfo statInfo : ll) {
+            System.out.println(statInfo);
+        }
 
         System.out.println("////////////");
     }
--- workpublish/bin/start.sh
+++ workpublish/bin/start.sh
@@ -1,11 +1,11 @@
 # debug
-cd /mnt/c/Projects/kc/minfs_student/workpublish/bin
-cd ..
+cd /root/project/minfs/workpublish/bin
 ## kill java
 ps -ef | grep java | grep -v grep | awk '{print $2}' | xargs kill -9
 ## run zks
-docker volume rm zk-register-v zk-master-meta-v zk-slave-meta-v
 docker container stop zk-register zk-master-meta zk-slave-meta
+docker container prune -f
+docker volume rm zk-register-v zk-master-meta-v zk-slave-meta-v
 
 docker volume create zk-register-v
 docker volume create zk-master-meta-v
@@ -16,23 +16,24 @@ docker run --name zk-master-meta -p 2182:2181 -v zk-mater-meta-v:/data --rm -d z
 docker run --name zk-slave-meta -p 2183:2181 -v zk-slave-meta-v:/data --rm -d zookeeper:3.9
 
 start_jar() {
-  jar_name=$1
-  log_name=$2
-  args=$3
+  jar_id=$1
+  jar_name=$2
+  log_name=$3
+  args=$4
 
-  pwd
+  echo "Starting $jar_id..."
   nohup java -jar $jar_name $args > $log_name 2>&1 &
   echo "$jar_name started successfully. pid: $!"
 }
 
 mkdir -p ./dataServer/log
-start_jar "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer1.log" "--server.port=9000 --az.rack=rack0 --az.zone=zone0"
-start_jar "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer2.log" "--server.port=9001 --az.rack=rack1 --az.zone=zone1"
-start_jar "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer3.log" "--server.port=9002 --az.rack=rack2 --az.zone=zone2"
-start_jar "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer4.log" "--server.port=9003 --az.rack=rack3 --az.zone=zone3"
+start_jar "dataServer1" "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer1.log" "--server.port=9000 --az.rack=rack0 --az.zone=zone0"
+start_jar "dataServer2" "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer2.log" "--server.port=9001 --az.rack=rack1 --az.zone=zone1"
+start_jar "dataServer3" "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer3.log" "--server.port=9002 --az.rack=rack2 --az.zone=zone2"
+start_jar "dataServer4" "./dataServer/dataServer-1.0.jar" "./dataServer/log/dataServer4.log" "--server.port=9003 --az.rack=rack3 --az.zone=zone3"
 
 mkdir -p ./metaServer/log
-start_jar "./metaServer/metaServer-1.0.jar" "./metaServer/log/metaServer1.log" "--server.port=8000"
+start_jar "metaServer-master" "./metaServer/metaServer-1.0.jar" "./metaServer/log/metaServer1.log" "--server.port=8000"
 sleep 3
-start_jar "./metaServer/metaServer-1.0.jar" "./metaServer/log/metaServer2.log" "--server.port=8001"
+start_jar "metaServer-slave" "./metaServer/metaServer-1.0.jar" "./metaServer/log/metaServer2.log" "--server.port=8001"
 
